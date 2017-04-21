f5c93784b4530622a3d76ac359793bb38124c377
ashok <uaagman05neu@gmail.com>
2017-04-21 08:04:05 -0500

    checkout init

--- src/main/java/com/webstore/model/User.java
+++ src/main/java/com/webstore/model/User.java
@@ -3,10 +3,28 @@ package com.webstore.model;
 public class User {
     private String username;
     private String password;
+    private BillingAddress billingAddress = null;
+    private ShippingAddress shippingAddress = null;
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+
+    public BillingAddress getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(BillingAddress billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public ShippingAddress getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(ShippingAddress shippingAddress) {
+        this.shippingAddress = shippingAddress;
     }
 
     public String getUsername() {
--- src/main/java/com/webstore/servlet/Checkout.java
+++ src/main/java/com/webstore/servlet/Checkout.java
@@ -1,17 +1,44 @@
 package com.webstore.servlet;
 
+import com.webstore.model.Factory;
+import com.webstore.model.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ashok on 4/20/2017.
  */
 public class Checkout extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        if(request.getSession().getAttribute("LoggedUser") == null){
+            response.sendRedirect(getServletContext().getContextPath()+"/login");
+        }else{
+            User logged = null;
+            List<User> users = new ArrayList<>(Factory.getUsers().values());
+            for(User u : users){
+                if(u.getUsername().equals(request.getSession().getAttribute("LoggedUser"))){
+                    logged = u;
+                    break;
+                }
+            }
+            if(logged != null) {
+                if (logged.getBillingAddress() == null) {
+                    //to get billing address
+                } else if (logged.getShippingAddress() == null) {
+                    //to get shipping address
+                } else {
+                    //to checkout details display page
+                }
+            } else{
+                response.sendRedirect(getServletContext().getContextPath()+"/logout");
+            }
+        }
     }
 }
